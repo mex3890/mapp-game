3c6514a23820640a5f4470982250496388ec1ada
Joao Wagner Lisboa Miranda <joaowagner.ciee@cedae.com.br>
2022-11-10 11:51:06 -0300

    New profile screen

---
+++ app/src/main/java/br/com/mappgame/NewProfileActivity.kt
@@ -0,0 +1,11 @@
+package br.com.mappgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_profile)
+    }
+}
